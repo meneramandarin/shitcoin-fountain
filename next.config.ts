a7d04a6a688b2a931dea511446aab449f37b6028
meneramandarin <Marlene.ronstedt@gmail.com>
2025-12-02 15:13:04 -0800

    Add aggressive webpack config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    
+    // Ignore test files
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'why-is-node-running': false,
+    }
+    
     return config
   },
+  // Transpile these packages
+  transpilePackages: ['@walletconnect/ethereum-provider'],
 };
 
 export default nextConfig;
