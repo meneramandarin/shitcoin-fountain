da0a40ea9d2f06bd3a1cc71b16e8c66ada7859f2
meneramandarin <Marlene.ronstedt@gmail.com>
2025-12-02 15:30:12 -0800

    webpackage

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    
-    // Ignore test files
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'why-is-node-running': false,
-    }
-    
-    return config
-  },
-  // Transpile these packages
-  transpilePackages: ['@walletconnect/ethereum-provider'],
+  /* config options here */
 };
-
 export default nextConfig;
