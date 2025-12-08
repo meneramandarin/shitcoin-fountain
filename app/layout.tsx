58dca51e17f369f863390cbe8a10b8f3ffda62e5
meneramandarin <Marlene.ronstedt@gmail.com>
2025-12-07 18:55:22 -0800

    vercel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Providers } from "./providers";
 import "./globals.css";
 import { Buenard } from "next/font/google";
@@ -49,6 +50,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={buenard.className}>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
